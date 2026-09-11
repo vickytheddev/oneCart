ce59edadd16c9790690abf8cccf0482f80ef19f6
Vicky kumar <148261348+vickytheddev@users.noreply.github.com>
2026-09-11 16:51:57 +0530

    Update CORS origin to include admin frontend

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
- origin:["https://onecart-frontend-dtdu.onrender.com" , "http://localhost:5174"],
+ origin:["https://onecart-frontend-dtdu.onrender.com" , "https://onecart-admin-g5hv.onrender.com"],
  credentials:true
 }))
 
