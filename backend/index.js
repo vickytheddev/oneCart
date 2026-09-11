616af50dcc2273e95127daaf92f1aad14c2431ba
Vicky kumar <148261348+vickytheddev@users.noreply.github.com>
2026-09-11 16:41:27 +0530

    Update CORS origin for production environment

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://onecart-frontend-dtdu.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
